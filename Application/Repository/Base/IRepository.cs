2cdb3d0ca5b28167cc81bf2557cb9f81a644d2cd
DanilaVdovenko <deelog123223@gmail.com@>
2025-11-17 01:59:34 +0700

    что-то

--- Application/Repository/Base/IRepository.cs
+++ Application/Repository/Base/IRepository.cs
@@ -3,4 +3,6 @@
 public interface IRepository<T> where T : class
 {
     IEnumerable<T> GetAll();
+
+    T Add(T entity);
 }
--- Infrastructure/Repository/UserRepository.cs
+++ Infrastructure/Repository/UserRepository.cs
@@ -12,5 +12,14 @@ public class UserRepository : IUserRepository
         _context = context;
     }
 
+    public User Add(User entity)
+    {
+        _context.Users.Add(entity);
+        _context.SaveChanges();
+
+        _context.Entry(entity).Reload();
+        return entity;
+    }
+
     public IEnumerable<User> GetAll() => [.. _context.Users];
 }
